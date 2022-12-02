acf6900bc865706a5aabe7f01bc7d6219128b444
emcnab <eliot.code@proton.me>
2022-12-02 12:00:53 +0100

    feat: added the ability to combine a format byte and a modifier byte into a 32 bit representation
    First byte  -> format
    Second byte -> modifers
    
    refactor!: cobined modifiers.c and formats.c into a single file formdata.c

--- includes/formdata.h
+++ includes/formdata.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   modifiers.h                                        :+:      :+:    :+:   */
+/*   formdata.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 09:33:13 by emcnab            #+#    #+#             */
-/*   Updated: 2022/12/02 11:22:26 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/12/02 11:58:43 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,24 @@
 # include "types.h"
 
 /**
- * @enum: t_modifier
- * @brief: represents the various modifers that be applied to printf.
+ * @enum t_format
+ * @brief represents a format character in a printf call.
+ */
+typedef enum e_format
+{
+	CHARACTER, /* used to display a single ascii charcter                     */ 
+	STRING,    /* used to display a string                                    */ 
+	POINTER,   /* used to display a pointer adress                            */ 
+	DECIMAL,   /* used to display a base 10 decimal number                    */ 
+	INT,       /* used to display a base 10 integer number                    */ 
+	UINT,      /* used to display a base 10 unsigned integer number           */ 
+	SMALL_HEX, /* used to display a hexadecimal number in small caps          */ 
+	BIG_HEX    /* used to display a hexadecimal number in big caps            */ 
+}	t_e_format;
+
+/**
+ * @enum t_modifier
+ * @brief represents the various modifers that be applied to printf.
  */
 typedef enum e_modifier
 {
@@ -27,12 +43,19 @@ typedef enum e_modifier
 	ALT_FORM,	/*! '#' : adds 0x for hex and .0 to floats                    */
 	FORCE_SIGN,	/*! '+' : display '+' in signed conversion                    */
 	BLANK_SIGN	/*! ' ' : blank before positive number in signed conversion   */
-}	t_modifier;
+}	t_e_modifier;
 
+/**
+ * @struct t_modinfo
+ * @brief Represents information attached to a modifier, used for comparison
+ * 	between modifiers and conversion to byte-form representation.
+ */
 typedef struct s_modinfo
 {
 	char	modifer;	/* byte pattern representing the modifier             */
 	char	conflics;	/* byte pattern representing uncompatible modifier    */
-}	t_modinfo;
+}	t_s_modinfo;
+
+char	ft_modify(char modgroup, t_e_modifier modifier);
 
 #endif
--- srcs/formdata.c
+++ srcs/formdata.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   modifiers.c                                        :+:      :+:    :+:   */
+/*   formdata.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/02 10:09:14 by emcnab            #+#    #+#             */
-/*   Updated: 2022/12/02 11:38:27 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/12/02 12:00:43 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/modifiers.h"
+#include "../includes/formdata.h"
 
-static const t_modinfo	g_info[6] = {
+static const t_s_modinfo	g_info[6] = {
 {0x1, 0x0},		/* '-' */
 {0x2, 0x1},		/* '0' */
 {0x4, 0x0},		/* '.' */
@@ -20,6 +20,35 @@ static const t_modinfo	g_info[6] = {
 {0x10, 0x0},	/* '+' */
 {0x20, 0x10}	/* '-' */
 };
+
+/**
+ * @brief Combines the [format] byte and modifier bytes into 32 bits, where the
+ * 	first byte corresponds to [format] and the second to [modgroup].
+ *
+ * @brief format (char): byte representation of the format to use in a printf
+ * 	call.
+ * @brief modgroup (char): byte representation of the modifiers used in a printf
+ * 	call.
+ *
+ * return (short int): 32-bit representation containing both the [format] and
+ * 	[modgroup] bytes.
+ */
+short int	ft_printf_data(char format, char modgroup)
+{
+	return ((format << 8) | modgroup);
+}
+
+/**
+ * @brief Converts a [format] to a single bit in a byte.
+ *
+ * @param format (t_format): the format to convert to a byte.
+ *
+ * @return (char): byte representation of [format].
+ */
+char	ft_format(t_e_format format)
+{
+	return (1 << format);
+}
 
 /**
  * @brief Adds a [modifier] to a [modgroup] if the [modgroup] does not already
@@ -30,9 +59,9 @@ static const t_modinfo	g_info[6] = {
  *
  * @return (char): modgroup with the new modifier if there were no comflicts.
  */
-char	ft_modify(char modgroup, t_modifier modifier)
+char	ft_modify(char modgroup, t_e_modifier modifier)
 {
-	t_modinfo	modinfo;
+	t_s_modinfo	modinfo;
 
 	modinfo = g_info[modifier];
 	if (!(modgroup & modinfo.conflics))
