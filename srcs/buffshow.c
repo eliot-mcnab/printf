131ed1f651ec612e532e930e118a68446cbab431
emcnab <eliot.code@proton.me>
2022-12-03 17:24:44 +0100

    fix: ft_buffshow no longuer mutates buffer index

--- srcs/buffshow.c
+++ srcs/buffshow.c
@@ -6,7 +6,7 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 15:57:28 by emcnab            #+#    #+#             */
-/*   Updated: 2022/12/03 17:20:30 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/12/03 17:24:26 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 void	ft_buffshow(t_s_buffer *buffer)
 {
+	size_t	i;
 	t_lword	*str_current;
 	t_lword	*str_previous;
 
 	if (!buffer)
 		return ;
+	i = buffer->i;
 	str_previous = buffer->data;
-	while (buffer->i > 0)
+	while (i > 0)
 	{
 		str_current = ft_lword_search(str_previous, '\0');
-		buffer->i -= (size_t)(str_current - buffer->data);
+		i -= (size_t)(str_current - buffer->data);
 		// TODO: ft_putstr_fd should inform of write failure
 		ft_putstr_fd((char *)str_previous, STDOUT);
 		str_previous = str_current++;
