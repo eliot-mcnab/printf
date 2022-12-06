eda6167acf6c440983f94123b90078c9b4f07c26
emcnab <eliot.code@proton.me>
2022-12-06 13:31:53 +0100

    docs: finished updating ft_printf to match doxygen standards

--- includes/printf.h
+++ includes/printf.h
@@ -6,9 +6,35 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 11:27:14 by emcnab            #+#    #+#             */
-/*   Updated: 2022/12/05 16:34:28 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/12/06 13:26:44 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file printf.h
+ * @brief Simple re-implementation of printf.
+ *
+ * Supports the following flags:
+ * - 'c': character
+ * - 's': string
+ * - 'd': decimal
+ * - 'i': int
+ * - 'u': unsigned int
+ * - 'x': small caps hex code
+ * - 'X': big caps hex code
+ * - '%': percentage escaping
+ *
+ * Supports the following modifiers:
+ * - '-': right-pads number with spaces
+ * - '0': left-pads number with zeros
+ * - '.': set floating-point precision
+ * - '#': adds 0x for hex and .0 for floats
+ * - '+': displays '+' in signed conversions
+ * - ' ': blank before positive number in signed conversion.
+ *
+ * @author Eliot Mcnab
+ * @date 12/06/2022
+ */
 
 #ifndef PRINTF_H
 # define PRINTF_H
@@ -18,4 +44,4 @@
 
 int	ft_printf(const char *str, ...);
 
-#endif
+#endif 
--- includes/types.h
+++ includes/types.h
@@ -6,9 +6,17 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/30 09:48:31 by emcnab            #+#    #+#             */
-/*   Updated: 2022/12/03 17:17:17 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/12/06 13:28:41 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+/**
+ * @file types.h
+ * @brief Universal typedefs used throughout ft_printf
+ *
+ * @author Eliot McNab
+ * @date 12/06/2022
+ */
 
 #ifndef TYPES_H
 # define TYPES_H
